3ff8136ae6481d3b3d97f347871cc99a7e249e3d
Minna Kakkonen <lenttis_94@hotmail.com>
2018-12-29 23:57:25 +0200

    Lisätty Manga luokka

---
+++ OmaMgKirjasto/src/omamgkirjasto/Manga.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package omamgkirjasto;
+
+/**
+ *
+ * @author lentt
+ */
+public class Manga {
+   private String nimi;
+    private String tekija;
+    private String kustantaja;
+    private String kieli; 
+    
+     public Manga (String annettuetunimi, String annettutekija, String annettukustantaja, String annettukieli){
+        this.nimi = annettuetunimi;
+        this.tekija = annettutekija;
+        this.kustantaja = annettukustantaja;
+        this.kieli = annettukieli;
+    } 
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public String getTekija() {
+        return tekija;
+    }
+
+    public void setTekija(String tekija) {
+        this.tekija = tekija;
+    }
+
+    public String getKustantaja() {
+        return kustantaja;
+    }
+
+    public void setKustantaja(String kustantaja) {
+        this.kustantaja = kustantaja;
+    }
+
+    public String getKieli() {
+        return kieli;
+    }
+
+    public void setKieli(String kieli) {
+        this.kieli = kieli;
+    }
+     
+    
+}
